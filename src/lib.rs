8c8c239df8e328dcc478057ece496ae6799e01dd
jiyinyiyong <jiyinyiyong@gmail.com>
2021-09-06 18:00:45 +0800

    handle warnings from clippy

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ fn create_dual_balanced_ternary(x: f64) -> DualBalancedTernary {
   while integral_part > 0 {
     let left = integral_part % 3;
     if left == 0 {
-      ()
+      // nothing
     } else if left == 1 {
       result = result.add_at(idx, Dbt3);
     } else if left == 2 {
@@ -36,7 +36,7 @@ fn create_dual_balanced_ternary(x: f64) -> DualBalancedTernary {
       unreachable!(format!("unexpected reminder: {} from {}", left, x))
     }
     integral_part = (integral_part - left) / 3;
-    idx = idx + 1;
+    idx += 1;
   }
 
   if negative_value {
@@ -48,13 +48,13 @@ fn create_dual_balanced_ternary(x: f64) -> DualBalancedTernary {
   let mut f_idx = -1;
   let mut precision = DIV_PRECISION; // TODO
   while fractional_part > 0.0 && precision > 0 {
-    fractional_part = fractional_part * 3.0;
+    fractional_part *= 3.0;
     let left = fractional_part.floor();
     if left == 0.0 {
-      ()
-    } else if left == 1.0 {
+      // nothing
+    } else if (left - 1.0).abs() < std::f64::EPSILON {
       result = result.add_at(f_idx, Dbt3);
-    } else if left == 2.0 {
+    } else if (left - 2.0).abs() < std::f64::EPSILON {
       result = result.add_at(f_idx + 1, Dbt3);
       result = result.add_at(f_idx, Dbt7);
     } else {
@@ -63,9 +63,9 @@ fn create_dual_balanced_ternary(x: f64) -> DualBalancedTernary {
         left, fractional_part
       ));
     }
-    fractional_part = fractional_part - left;
-    f_idx = f_idx - 1;
-    precision = precision - 1;
+    fractional_part -= left;
+    f_idx -= 1;
+    precision -= 1;
   }
   result
 }
@@ -79,5 +79,5 @@ pub fn create_dual_balanced_ternary_from_pair(x: f64, y: f64) -> DualBalancedTer
   for (idx, item) in b.fractional.to_owned().iter().enumerate() {
     b.fractional[idx] = item.flip_xy();
   }
-  return a + b;
+  a + b
 }
